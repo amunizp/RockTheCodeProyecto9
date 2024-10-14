3080414aa7dab65252ae615c095c26e17860c931
Andres Muniz Piniella <amp-git@c4ad.eu>
2024-10-14 16:39:19 +0100

    sweeped through pages seems to skip the last one and double up.

--- src/utils/scrapper.js
+++ src/utils/scrapper.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
+const { TIMEOUT } = require('dns')
 
 const componentArray = []
+let pageCount = 1
 
 const Scrapper = async (url) => {
   try {
@@ -19,18 +21,67 @@ const Scrapper = async (url) => {
 }
 const sweepPage = async (page, browser) => {
   console.log('Starting sweep of page')
-
-  const Navlist = await page.$$(
+  const listDiv = await page.$$(
     '#navlist > li > div[class="s-productthumbbox"]'
   )
-  const img = await page.$$eval(
-    '#navlist > li > div[class="s-productthumbbox"] ',
-    (elements) => {
-      console.log('These are the elements', elements)
-      return elements //.map((e) => e.src)
+  for (const divProduct of listDiv) {
+    let img = await divProduct.$eval('img', (element) => element.src)
+    let title = await divProduct.$eval('img', (element) => element.title)
+
+    let description = await divProduct.$eval(
+      'span.productdescriptionname',
+      (element) => element.textContent
+    )
+    try {
+      // page.waitForSelector('span[class="CurrencySizeLarge curprice"]')
+      let price = await divProduct.$eval('span.CurrencySizeLarge', (element) =>
+        element.textContent.trim().replace('£', '')
+      )
+      const component = { img, title, description, price }
+      componentArray.push(component)
+    } catch (error) {
+      console.log('error getting the price')
+      console.error(error)
+      console.log(componentArray[componentArray.length - 1])
     }
-  )
-  console.log('The images are:', img)
+  }
+  try {
+    await page.waitForSelector('a[class="swipeNextClick NextLink "]', {
+      timeout: 60000
+    })
+    console.log('waited for next page button')
+    await page.$eval('a[class="swipeNextClick NextLink "]', (el) => el.click())
+    console.log('clicked the button')
+    // await page.waitForNavigation({
+    //   timeout: 60000
+    //   // waitUntil: 'domcontentloaded'
+    // }) //wait for navigation timed out incresed to 60s!
+    // console.log('waited for navigation')
+    await page.waitForSelector('#navlist', { timeout: 60000 })
+    let currentPage = await page.$eval(
+      'span[class="CurrentPageNumber"]',
+      (element) => element.textContent
+    )
+    pageCount += 1
+
+    console.log('the current page is ', currentPage, 'it should be', pageCount)
+
+    sweepPage(page, browser)
+  } catch (error) {
+    console.error(error)
+    console.log(
+      'finished all pages and got an error for not finding the next one',
+      error
+    )
+    console.log(componentArray[componentArray.length - 1])
+    saveProducts(componentArray)
+    await browser.close()
+  }
+}
+const saveProducts = (arrayOfObjects) => {
+  fs.writeFile('products.json', JSON.stringify(arrayOfObjects), () => {
+    console.log('File Saved')
+  })
 }
 
 module.exports = { Scrapper }
